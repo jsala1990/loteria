82c9cb21a64e71993ae5f9f1e103309eace9a1e7
pen15 <pen15@itsallagame.com>
2017-08-18 15:23:45 -0400

    adding random call

--- spec/BingoGridSpec.js
+++ spec/BingoGridSpec.js
@@ -1,28 +1,39 @@
 describe('BingoGrid', function() {
   var BingoGrid = require('../lib/BingoGrid');
-  var grid;
 
-  it('should initialize itself with rows and cols', function() {
-    grid = new BingoGrid(2, 3);
-    expect(grid instanceof BingoGrid).toBeTruthy();
-    expect(grid.rows).toEqual(2);
-    expect(grid.cols).toEqual(3);
+  describe('#new', function() {
+    it('should initialize itself with rows and cols', function() {
+      var grid = new BingoGrid(2, 3);
+      expect(grid instanceof BingoGrid).toBeTruthy();
+      expect(grid.rows).toEqual(2);
+      expect(grid.cols).toEqual(3);
+    });
+
+    it('should throw an error for invalid columns', function() {
+      var badBingoInit = function() {
+        new BingoGrid(0, 1);
+      }
+
+      expect(badBingoInit).toThrow(new Error('Rows must be positive integer'));
+    });
+
+    it('should throw an error for invalid rows', function() {
+      var badBingoInit = function() {
+        new BingoGrid(1, 0);
+      }
+
+      expect(badBingoInit).toThrow(new Error('Cols must be positive integer'));
+    });
   });
 
-  it('should throw an error for invalid columns', function() {
-    var badBingoInit = function() {
-      new BingoGrid(0, 1);
-    }
+  describe('#random', function () {
+    it('throw an exception if you need more words', function() {
+      var grid = new BingoGrid(2,1);
+      var badRandomCall = function() {
+        grid.random(['oneWord']);
+      };
 
-    expect(badBingoInit).toThrow(new Error('Rows must be positive integer'));
+      expect(badRandomCall).toThrow(new Error('need more words'));
+    });
   });
-
-  it('should throw an error for invalid rows', function() {
-    var badBingoInit = function() {
-      new BingoGrid(1, 0);
-    }
-
-    expect(badBingoInit).toThrow(new Error('Cols must be positive integer'));
-  });
-
 });
