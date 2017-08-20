cf54ed3a3338f4a9c907f2d90ee97a3b25fc659d
jsalazar <j@email.com>
2017-08-20 00:06:16 -0400

    adding readme

---
+++ README.md
@@ -0,0 +1,2 @@
+links following:
+https://www.phpied.com/3-ways-to-define-a-javascript-class/
--- spec/BingoGridSpec.js
+++ spec/BingoGridSpec.js
@@ -11,29 +11,49 @@ describe('BingoGrid', function() {
 
     it('should throw an error for invalid columns', function() {
       var badBingoInit = function() {
-        new BingoGrid(0, 1);
+        new BingoGrid(1, 2);
       }
 
-      expect(badBingoInit).toThrow(new Error('Rows must be positive integer'));
+      expect(badBingoInit).toThrow(new Error('Rows must be greater than 1'));
     });
 
     it('should throw an error for invalid rows', function() {
       var badBingoInit = function() {
-        new BingoGrid(1, 0);
+        new BingoGrid(2, 1);
       }
 
-      expect(badBingoInit).toThrow(new Error('Cols must be positive integer'));
+      expect(badBingoInit).toThrow(new Error('Cols must be greater than 1'));
     });
   });
 
   describe('#random', function () {
-    it('throw an exception if you need more words', function() {
+    it('throws an exception if you need more words', function() {
       var grid = new BingoGrid(2,1);
       var badRandomCall = function() {
         grid.random(['oneWord']);
       };
 
       expect(badRandomCall).toThrow(new Error('need more words'));
+    });
+
+    it('returns 1x1 array', function() {
+      var grid = new BingoGrid(1,1);
+      var listOfWords = ['word1'];
+
+      expect(grid.random(listOfWords)).toEqual(['word1']);
+    });
+
+    it('returns 2x2 array', function() {
+      var grid = new BingoGrid(2,1);
+      var listOfWords = ['word1', 'word2'];
+
+      var randomGrid = grid.random(listOfWords);
+
+      var numberOfColumns = randomGrid.length
+      var numberOfRows = randomGrid[0].length
+
+      expect(numberOfColumns).toEqual(2);
+      expect(numberOfRows).toEqual(1);
     });
   });
 });
